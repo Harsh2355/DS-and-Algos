e711a68a06f02516b4ee5b9e404d6c7054fae1f6
Harshvardhan Kakar <harshvardhankakar@gmail.com>
2022-08-09 21:11:14 -0400

    Has Path Latest

--- chapter4/hasPath.java
+++ chapter4/hasPath.java
@@ -5,45 +5,27 @@ class Solution {
         if (n == 1) {
             return true;
         }
-        Queue<Integer> theQueue = new PriorityQueue<Integer>();
-        Map<Integer, Boolean> visited = new LinkedHashMap<Integer, Boolean>();
-        Map<Integer, ArrayList<Integer>> graph = new LinkedHashMap<Integer, ArrayList<Integer>>();
-        for (int[] ele : edges) {
-            if (!visited.containsKey(ele[0])) {
-                visited.put(ele[0], false);
-                graph.put(ele[0], new ArrayList<Integer>());
-            }
-            ArrayList<Integer> curNeighbours1 = graph.get(ele[0]);
-            curNeighbours1.add(ele[1]);
-            graph.put(ele[0], curNeighbours1);
-            
-            if (!visited.containsKey(ele[1])) {
-                visited.put(ele[1], false);
-                graph.put(ele[1], new ArrayList<Integer>());
-            }
-            ArrayList<Integer> curNeighbours = graph.get(ele[1]);
-            curNeighbours.add(ele[0]);
-            graph.put(ele[1], curNeighbours);
-            
-            if (ele[0] == source) {
-                theQueue.add(ele[1]);
-            }
-            else if (ele[1] == source) {
-                theQueue.add(ele[0]);
-            }
+        Queue<Integer> theQueue = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int i = 0; i < edges.length; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
         }
         
         // BFS
-        visited.put(source, true);
-        while (theQueue.peek() != null) {
-            Integer curVisiting = theQueue.remove();
+        visited.add(source);
+        theQueue.add(source);
+        while (!theQueue.isEmpty()) {
+            int curVisiting = theQueue.poll();
+            visited.add(curVisiting);
             if (curVisiting == destination) {
                 return true;
             }
-            visited.put(curVisiting, true);
-            ArrayList<Integer> neighbours = graph.get(curVisiting);
-            for (Integer ele : neighbours) {
-                if (!visited.get(ele)) {
+            for (Integer ele : adj.get(curVisiting)) {
+                if (!visited.contains(ele)) {
                     theQueue.add(ele);
                 }
             }
